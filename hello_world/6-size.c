ac5e5168cd6808011d8a250ba400c7f05cd69aaa
EJMarkle <emarkle99@gmail.com>
2023-05-24 16:53:52 -0700

    6 fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,10 @@
  */
 int main(void)
 {
-	printf("sizeof int is %zu\n", sizeof(char));
-	printf("sizeof int is %zu\n", sizeof(int));
-	printf("sizeof int is %zu\n", sizeof(long));
-	printf("sizeof int is %zu\n", sizeof(long long));
-	printf("sizeof int is %zu\n", sizeof(float));
-	printf("sizeof int is %zu\n", sizeof(double));
+	printf("Size of a char: %zu\n", sizeof(char));
+	printf("Size of an int: %zu\n", sizeof(int));
+	printf("Size of a long int: %zu\n", sizeof(long));
+	printf("Size of a long long int: %zu\n", sizeof(long long));
+	printf("Size of a float: %zu\n", sizeof(float));
 	return (0);
 }

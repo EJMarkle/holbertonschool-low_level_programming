e14973a2932096bcbaa831ce117cfca5bbf84dd7
EJMarkle <emarkle99@gmail.com>
2023-05-24 15:49:32 -0700

    Betty.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-int main(void)
-/* The main function prints the message! */
+int main(void) /* The main function prints the message! */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

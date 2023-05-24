f634821aea71972c5942b5671e15dc4ea6689562
EJMarkle <emarkle99@gmail.com>
2023-05-24 16:28:06 -0700

    5, fixed?

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - prints dont puts
  * Return: nill
- * */
+ **/
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }

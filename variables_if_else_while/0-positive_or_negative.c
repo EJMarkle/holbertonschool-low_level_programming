5d5ff042ba40731b3e13e65fea8002e43f01fb11
EJMarkle <emarkle99@gmail.com>
2023-05-25 11:23:26 -0700

    0!

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+# include <stdlb.h>
+# include <time.h>
+/**
+ * main - gen random number
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if
+		(n > 0)
+		printf("is positive")
+	else
+		(n = 0)
+		printf("is zero")
+	else
+		(n < 0)
+		printf("is negative")
+	return (0);
+}

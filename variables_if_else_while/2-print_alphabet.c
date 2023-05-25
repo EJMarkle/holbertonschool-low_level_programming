b57f81b23a957d77737baaab37a12483cbf0d75f
EJMarkle <emarkle99@gmail.com>
2023-05-25 14:20:19 -0700

    2!

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints every character of the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for
+		(c = 'a';
+		 c <= 'z';
+		 ++c)
+	{
+		putchar(c);
+		putchar(' ');
+	}
+	return (0);
+}

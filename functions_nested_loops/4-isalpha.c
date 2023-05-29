7afa91b2d459e8c02733b93cbe21464d022180cd
EJMarkle <emarkle99@gmail.com>
2023-05-29 12:34:36 -0700

    4?

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - checks for betic char
+ * Return: 1 if c is a letter, 0 otherwise
+ * @c; char to check
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c > 'A' $$ c <= 'Z'));
+}

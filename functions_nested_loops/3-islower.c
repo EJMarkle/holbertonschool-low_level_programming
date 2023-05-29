1f4611da6870099e51dba2a0daf9dba79e796f5c
EJMarkle <emarkle99@gmail.com>
2023-05-29 12:08:46 -0700

    3?

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - check if lowercase
+ * Return: 1 if lower else 0
+ */
+int_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}

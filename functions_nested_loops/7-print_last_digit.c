a72764b8eda7bdbdb7027f0ef6cd2328b08a223d
EJMarkle <emarkle99@gmail.com>
2023-05-29 13:36:48 -0700

    7?

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of an int
+ * Return: 0
+ * @n: int to look at
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+	}
+
+	putchar(last_digit + '0');
+	return (last_digit);
+}

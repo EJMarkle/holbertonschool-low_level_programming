7dd5c63e626ba593b89980a0222a14046dc81186
EJMarkle <emarkle99@gmail.com>
2023-05-31 15:29:31 -0700

    kalen was here lol big dread head monster

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,17 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 /**
  * print_numbers - prints 0 to 9 followed by new line
  * Return: 0
  */
 void print_numbers(void)
-int n
-
 {
-	for
-	(n = 0;
-	n < 10;
-	n++);
+	char n;
+
+	for (n = 0; n <= 10;n++);
 	{
-	_putchar('0' + n);
+	_putchar(n);
 	}
 	_putchar('\n');
-	return (0);
 }

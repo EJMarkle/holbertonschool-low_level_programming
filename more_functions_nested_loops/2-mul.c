2e28d372a3f28c3afb14a13d37dacd5ba6164f02
EJMarkle <emarkle99@gmail.com>
2023-05-31 13:32:28 -0700

    2?

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiples 2 ints
+ * Return: 0
+ * @a: first int to be mult'd
+ * @b: second int to be muly'd
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

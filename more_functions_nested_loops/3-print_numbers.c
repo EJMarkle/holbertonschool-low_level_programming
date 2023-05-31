da8c3252567afdfb7d99e5e50dd2114b720bebf4
EJMarkle <emarkle99@gmail.com>
2023-05-31 14:56:02 -0700

    3...

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints 0 to 9 followed by new line
+ * Return: 0
+ */
+void print_numbers(void)
+int n
+
+{
+	for
+	(n = 0;
+	n < 10;
+	n++);
+	{
+	_putchar('0' + n);
+	}
+	_putchar('\n');
+	return (0);
+}

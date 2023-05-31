fbb2580522631afc475a2488ad2ea28c54c0b000
EJMarkle <emarkle99@gmail.com>
2023-05-31 16:39:28 -0700

    5?

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - prints 0-14 10 times
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+int n;
+int c;
+
+for (c = 0; c <= 10; c++)
+{
+	for (n = 0; n <= 14; n++)
+	{
+		if (n > 9)
+		{
+			_putchar('0' + (c / 10));
+		}
+		_putchar('0' + (c / 10));
+	}
+	_putchar('\n');
+}
+}

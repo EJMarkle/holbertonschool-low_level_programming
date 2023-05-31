9ed4102e8e4e54a4b1fa21439f6d790932056862
EJMarkle <emarkle99@gmail.com>
2023-05-31 13:22:20 -0700

    1?

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - checks if a char is a dig
+ * Return: 1 if c is a dig 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if
+	(c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}

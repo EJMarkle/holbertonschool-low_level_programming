e31234a3de6076515e8a0b941eab1dc3a733aeca
EJMarkle <emarkle99@gmail.com>
2023-06-01 19:43:34 -0700

    8?

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - prints a square
+ * Return: 0
+ * @size: size of da square
+ */
+void print_square(int size)
+{
+int i;
+int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('#');
+		}
+	}
+}

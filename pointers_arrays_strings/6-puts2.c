155f323296df9a036a94dcb82215d515533d7c0b
EJMarkle <emarkle99@gmail.com>
2023-06-05 11:21:38 -0700

    6?

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - prints evey other char in a string
+ * @str: string to be fed
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+}

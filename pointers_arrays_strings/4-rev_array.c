58ddfa4646e8f9161fed97d8f8cf299cc4659c05
EJMarkle <emarkle99@gmail.com>
2023-06-09 15:17:59 -0700

    4?

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of ints
+ * @n: # of elements in the array
+ * @a: array to be swapped
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int length = strlen(s);
+
+	while (length > 0)
+	{
+		_putchar(s[length - 1]);
+		length--;
+	}
+	_putchar('\n');
+}

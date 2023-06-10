1cf00acdba410fe37d555a72a3b8c2f72c037ce2
EJMarkle <emarkle99@gmail.com>
2023-06-10 16:11:27 -0700

    1?

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_memcpy - copies n bytes from src to dest
+ * Return: pointer to dest
+ * @n: bytes of memory to be copied
+ * @src: memory area to be copied from
+ * @dest: memory area to be copied to
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dest_start = dest;
+	char *src_start = src;
+
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	return (dest_start);
+}

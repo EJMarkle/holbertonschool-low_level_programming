c9df0a0578cd697e4055a5736a9c88d37790fb7e
EJMarkle <emarkle99@gmail.com>
2023-06-11 14:51:49 -0700

    8?

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_diagsums - prints sum of 2 diagnals of a square matrix of ints
+ * @a: array that hold the sums
+ * @size: size of matrix
+ * @suma: sum of first diag
+ * @sumb: sum of second diag
+ * Return: 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int cnt;
+	int suma = 0;
+	int sumb = 0;
+
+	for (cnt = 0; cnt < size; cnt++)
+	{
+		suma += a[cnt * size + cnt];
+	}
+	for (cnt = 0; cnt < size; cnt++)
+	{
+		sumb += a[cnt * size + (size - 1 - cnt)];
+	}
+	printf("%d, %d\n", suma, sumb);
+}

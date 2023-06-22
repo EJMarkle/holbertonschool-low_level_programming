633c0fd6090334632ffcca1fe820da75f651a2f2
EJMarkle <emarkle99@gmail.com>
2023-06-22 11:43:29 -0700

    updated main.h

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * *create_array - creates an array of chars, and initializes it with a specific char
+ * Return: pointer to the array, or NULL if size = 0 or fails
+ */
+
+char *create_array(unsigned int size, char c);
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef header_h
 #define header_h
+#include <stdio.h>
+#include <stdlib.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -36,5 +38,10 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 void _print_rev_recursion(char *s);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif

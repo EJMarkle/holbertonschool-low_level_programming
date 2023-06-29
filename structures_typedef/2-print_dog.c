f314d98cc69da2070d4d5a895dfae9dca7374dd1
EJMarkle <emarkle99@gmail.com>
2023-06-29 12:19:03 -0700

    2 print dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,4 +4,12 @@
  * print_dog - prints the dog struct
  * @d: pointer to dog
  */
-
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)";
+		printf("Age: %.1f\n", d->age);
+		printf("Owner: %s\n", d->owner != ? d->owner : "(nil)";
+	}
+}

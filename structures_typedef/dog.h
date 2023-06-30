4c095013a54f3b9d88e3285b3160228daa1742d1
EJMarkle <emarkle99@gmail.com>
2023-06-30 13:25:46 -0700

    3???

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,14 +7,14 @@
  * @owner: owner of dog
  */
 
-typedef dog dog_t;
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

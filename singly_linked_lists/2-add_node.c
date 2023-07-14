33c0e4fa153f96f581d6098e192ace43c4a1d1da
EJMarkle <emarkle99@gmail.com>
2023-07-14 14:25:08 -0700

    add node comments

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,21 +8,25 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newnode;
+	list_t *newnode; /* newnode is a pointer to new node */
 
+	/* finds size of type list_t, and allocates memory for newnode */
 	newnode = malloc(sizeof(list_t));
 	if (newnode == NULL)
 		return (NULL);
 
+	/* duplicates str, and assigns it to member str of newnode */
 	newnode->str = strdup(str);
 	if (newnode->str == NULL)
 	{
-		free(newnode);
+		free(newnode); /* if str is NULL, free memory */
 		return (NULL);
 	}
+	/* strlen finds length of str, and sets it to len member of newnode */
 	newnode->len = strlen(str);
+	/* sets next member of newnode to head node */
 	newnode->next = *head;
-
+	/* sets *head to newnode */
 	*head = newnode;
 	return (newnode);
 }

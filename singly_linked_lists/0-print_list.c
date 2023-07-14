6164605b20015b2156e5ac383479ebfb69f7cf7f
EJMarkle <emarkle99@gmail.com>
2023-07-14 12:49:48 -0700

    0 first draft

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * Return: number of nodes, 0 if null
+ * @h: pointer to head node
+ */
+
+size_t print_list(const list_t *h);
+{
+	size_t nodecount = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		nodecount++;
+	}
+	return (nodecount);
+}

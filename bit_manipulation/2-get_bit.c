f5efc4c317bfe362816ad3a132cccd058faeac9c
EJMarkle <emarkle99@gmail.com>
2023-07-15 14:31:40 -0700

    get bit>

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -6,8 +6,17 @@
  * Return: value of bit at index or -1 if error
  */
 
-int get_bit(unsigned long int n, unsingned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask;
 
 	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	mask = 1UL << index;
+
+	if (n & mask)
+		return (1);
+	else
+		return (0);
+}

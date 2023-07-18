0d1f072100656c71ef40303d7ac52020704b2d4b
EJMarkle <emarkle99@gmail.com>
2023-07-17 18:17:22 -0700

    buh

--- file_io/main.h
+++ file_io/main.h
@@ -6,9 +6,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int open_file_from(const char *filename);
+int open_file_to(const char *filename);
+void copy_file(int fd_from, int fd_to);
+void print_error(char *msg);
 
 #endif
